fbc4d7346603866ece7896a3f8516e654bfd9c82
LeAnh <sinhla213@gmail.com>
2023-10-16 21:07:34 +0700

    update for pdf

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
       <div className="text-center text-6xl">
         <a
           className="animate__animated animate__heartBeat animate__infinite"
-          href="https://drive.google.com/file/d/1wtdtiBkdtzW0jkRYGCUKIbQkIJbcIRTY/view?usp=sharing"
+          href="https://drive.google.com/file/d/1wu5HAvjOYUQILJy9r3qDz1IaukO4MxOq/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
